530025b6da4063839e07d56391db012abf8027f3
iltonmi <linweili666@163.com>
2021-03-02 11:30:17 +0800

    remove redundant generated files

--- moz-todo-react/src/index.js
+++ moz-todo-react/src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <App subject="Clarice" />,
+  <App />,
   document.getElementById('root')
 );
 
