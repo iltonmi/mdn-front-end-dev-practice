1bb2fa5011c81f6e53f68b5e54bf8fe92918c021
iltonmi <linweili666@163.com>
2021-03-02 16:30:36 +0800

    give todo item uni-id, made heading text be dynamic

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton  from "./components/FilterButton";
-
-function addTask(name) {
-  alert(name);
-}
+import { nanoid } from "nanoid";
 
 function App(props) {
-  const taskList = props.tasks.map(task => (
+  const [tasks, setTasks] = useState(props.tasks);
+
+  function addTask(name) {
+    const newTask = {id: "todo-" + nanoid(), name: name, completed: false};
+    setTasks([...tasks, newTask]);
+  }
+
+  const taskList = tasks.map(task => (
     <Todo 
       id={task.id} 
       name={task.name} 
@@ -16,6 +20,10 @@ function App(props) {
       key={task.id}
     />
   ));
+  
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -28,7 +36,7 @@ function App(props) {
         <FilterButton />
       </div>
       <h2 id="list-heading">
-        3 tasks remaining
+        {headingText}
       </h2>
       <ul
         role="list"
